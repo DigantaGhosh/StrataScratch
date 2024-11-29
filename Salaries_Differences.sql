bc5375a90cff2e7e56b297bc80496b02a537c644
Diganta_Ghosh <ghoshdiganta1997@gmail.com>
2024-11-29 23:35:48 +0530

    Update and rename Salaries Differences to Salaries_Differences.sql

--- Salaries_Differences.sql
+++ Salaries_Differences.sql
@@ -1,9 +1,8 @@
-Calculates the difference between the highest salaries in the marketing and engineering departments. Output just the absolute difference in salaries.
+'''Calculates the difference between the highest salaries in the marketing and engineering departments. Output just the absolute difference in salaries.'''
 
-```SQL
+
 select abs(max(case when d2.department = 'marketing' then d1.salary end)-max(case when d2.department = 'engineering' then d1.salary end)) as salary_diff 
 from db_employee d1
 left join 
 db_dept d2 on d1.department_id=d2.id 
-where d2.department in ('marketing','engineering')
-```
+where d2.department in ('marketing','engineering');
