531958d3bee6524500f4594ff852468d92c484f8
Diganta_Ghosh <ghoshdiganta1997@gmail.com>
2024-12-01 19:32:43 +0530

    Update Reviews of Hotel Arena.sql

--- Reviews of Hotel Arena.sql
+++ Reviews of Hotel Arena.sql
@@ -1,1 +1,4 @@
 --Find the number of rows for each review score earned by 'Hotel Arena'. Output the hotel name (which should be 'Hotel Arena'), review score along with the corresponding number of rows with that score for the specified hotel.
+
+select hotel_name,reviewer_score,count(*) from hotel_reviews 
+where hotel_name like '%Hotel Arena%' group by hotel_name,reviewer_score;
