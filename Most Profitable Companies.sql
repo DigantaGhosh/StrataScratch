253137a431e5ec9f173c36b22c2a9c6f3309ca1e
Diganta_Ghosh <ghoshdiganta1997@gmail.com>
2024-12-01 21:16:32 +0530

    Update Most Profitable Companies.sql

--- Most Profitable Companies.sql
+++ Most Profitable Companies.sql
@@ -1,2 +1,7 @@
 --Find the 3 most profitable companies in the world. Sort the result based on profits in descending order. If multiple companies have the same profit, assign them the same rank and include all tied companies in the top results. Output the result along with the corresponding company name.
 
+select company,profits from 
+  ( select company,profits,dense_rank() over (order by profits desc) as dn 
+  from forbes_global_2010_2014) a 
+  where a.dn in (1,2,3) 
+  order by profits desc;
