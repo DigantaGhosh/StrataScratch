55a3add6e23b2f41e77c83e80def8cdc95cd193c
Diganta_Ghosh <ghoshdiganta1997@gmail.com>
2024-12-01 21:07:19 +0530

    Create Most Profitable Companies.sql

---
+++ Most Profitable Companies.sql
@@ -0,0 +1,2 @@
+--Find the 3 most profitable companies in the world. Sort the result based on profits in descending order. If multiple companies have the same profit, assign them the same rank and include all tied companies in the top results. Output the result along with the corresponding company name.
+
